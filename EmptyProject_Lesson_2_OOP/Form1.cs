fa4f9cf7b8dc5854e6763e4f8ebe0f3cf39fa9a2
Кирилл <kattoshkin@gmail.com>
2021-07-17 16:36:37 +0700

    Добавьте файлы проекта.

---
+++ EmptyProject_Lesson_2_OOP/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmptyProject_Lesson_2_OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Person genDirector = new Person();
+            genDirector.lastName = "Ponasenkov";
+            genDirector.firstName = "Evgeniy";
+            genDirector.patronymic = "Nicolaevich";
+
+            genDirector.birthDay = DateTime.Today;
+            genDirector.salary = 100000;
+            genDirector.inStaff = true;
+
+            Person officeWorker = new Person("Ponasenkov", "Evgeniy", "Nicolaevich",DateTime.Today , 100000, true);
+            decimal k = 1.2m;
+            string output = $"Зарплата officeWorker до индексации:{officeWorker.salary}\n";
+            officeWorker.indexingSalary(k);
+            output += $"Зарплата officeWorker после индексации:{officeWorker.salary}";
+            MessageBox.Show(output);
+        }
+    }
+}
---
+++ EmptyProject_Lesson_2_OOP/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyProject_Lesson_2_OOP
+{
+    public class Person
+    {
+        //fields
+        public string lastName;
+        public string firstName;
+        public string patronymic;
+
+        public DateTime birthDay;
+        public decimal salary;
+        public bool inStaff;
+
+        //constructors
+        public Person() { }
+
+        public Person(string lname, string fname, string patronym, DateTime birthDayTmp, decimal salaryTmp, bool inStaffTmp) {
+            this.lastName = lname;
+            this.firstName = fname;
+            this.patronymic = patronym;
+            this.birthDay = birthDayTmp;
+            this.salary = salaryTmp;
+            this.inStaff = inStaffTmp;
+        }
+
+        //methods
+        public void indexingSalary(decimal coef){
+            this.salary *= coef;
+        }
+    }
+}
